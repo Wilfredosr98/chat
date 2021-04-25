d3061407ceb0e91b6432f4d7fd3d4cea3383a65c
wilfredo <wilfredosr98@gmail.com>
2021-04-25 15:07:41 -0400

    segundo

--- src/database.js
+++ src/database.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://wilfre:wilfre@cluster0.zgvyq.mongodb.net/test', {
+mongoose.connect('mongodb+srv://wilfre:<password>@cluster0.zgvyq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
   .then(db => console.log('db connected'))
   .catch(err => console.log(err));
+
+
+  
